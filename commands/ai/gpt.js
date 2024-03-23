bcfea154b98d92573eec791eb37070ec7d5d2828
Fortunatus Mokaya ☠️ <nyangichafortunatus@gmail.com>
2024-03-23 15:32:22 +0300

    Add files via upload

--- commands/ai/gpt.js
+++ commands/ai/gpt.js
@@ -1,1 +1,31 @@
-//
+import { G4F } from "g4f"; 
+
+
+const gptA = async(client, m, text) => {
+
+if (!text) return m.reply("Provide text...");
+
+
+
+
+const GPT = new G4F(); 
+
+const messages = [
+	{ role: "system", content: "You're an AI.."},
+	{ role: "user", content: text}
+];
+
+
+GPT.chatCompletion(messages)
+  .then(result => {
+    console.log(result);
+    m.reply(result);
+  });
+
+
+
+
+
+}
+
+export default gptA;

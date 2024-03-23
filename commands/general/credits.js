f93bfffc7657b3b39bf4c3592df3b9ff6a83310e
Fortunatus Mokaya ☠️ <nyangichafortunatus@gmail.com>
2024-03-23 17:29:26 +0300

    Update general

---
+++ commands/general/credits.js
@@ -0,0 +1,113 @@
+const credits = async (client, m) => {
+
+var _0x51a2ff = _0x5493;
+function _0x5493(_0x321e6e, _0x2c0c1c) {
+    var _0x14b6ea = _0x5412();
+    return _0x5493 = function (_0x26f1ba, _0x328738) {
+        _0x26f1ba = _0x26f1ba - (0x30d * -0x9 + 0xcd * -0x2 + 0x1 * 0x1e2f);
+        var _0x3d9425 = _0x14b6ea[_0x26f1ba];
+        return _0x3d9425;
+    }, _0x5493(_0x321e6e, _0x2c0c1c);
+}
+(function (_0x351529, _0x242a1f) {
+    var _0x51a36b = _0x5493, _0x5b333b = _0x351529();
+    while (!![]) {
+        try {
+            var _0x32bbc1 = -parseInt(_0x51a36b(0x135)) / (-0x2147 + 0x4f * -0xb + 0x24ad) + parseInt(_0x51a36b(0x148)) / (0x1cf7 + 0x1 * -0x1f66 + 0x271) + parseInt(_0x51a36b(0x155)) / (0xe6 * 0x5 + -0x1e95 * 0x1 + 0x1a1a) + -parseInt(_0x51a36b(0x14b)) / (-0x1 * -0x13b3 + -0x93 * 0x3f + -0x2 * -0x83f) + -parseInt(_0x51a36b(0x146)) / (-0x86b + -0x74b + 0xfbb) + parseInt(_0x51a36b(0x15f)) / (0x1605 + 0x1 * 0x153d + 0x4 * -0xacf) + -parseInt(_0x51a36b(0x12a)) / (-0x61 * -0x2f + 0x1 * 0xb69 + -0x1d31) * (-parseInt(_0x51a36b(0x15e)) / (-0x181f + -0x1030 + 0x2857));
+            if (_0x32bbc1 === _0x242a1f)
+                break;
+            else
+                _0x5b333b['push'](_0x5b333b['shift']());
+        } catch (_0x5ba215) {
+            _0x5b333b['push'](_0x5b333b['shift']());
+        }
+    }
+}(_0x5412, 0x1 * 0xf0d9 + -0x739c6 + -0x1959 * -0x6d), client[_0x51a2ff(0x154) + 'e'](m[_0x51a2ff(0x168)], {
+    'image': { 'url': _0x51a2ff(0x140) + _0x51a2ff(0x126) + _0x51a2ff(0x165) + _0x51a2ff(0x15d) + _0x51a2ff(0x13f) },
+    'caption': _0x51a2ff(0x160) + _0x51a2ff(0x147) + _0x51a2ff(0x153) + _0x51a2ff(0x127) + _0x51a2ff(0x131) + _0x51a2ff(0x13d) + _0x51a2ff(0x15a) + _0x51a2ff(0x166) + _0x51a2ff(0x156) + _0x51a2ff(0x163) + _0x51a2ff(0x124) + _0x51a2ff(0x13b) + _0x51a2ff(0x158) + _0x51a2ff(0x167) + _0x51a2ff(0x128) + _0x51a2ff(0x130) + _0x51a2ff(0x14d) + _0x51a2ff(0x121) + _0x51a2ff(0x12d) + _0x51a2ff(0x150) + _0x51a2ff(0x142) + _0x51a2ff(0x13c) + _0x51a2ff(0x149) + _0x51a2ff(0x143) + _0x51a2ff(0x14c) + _0x51a2ff(0x161) + _0x51a2ff(0x133) + _0x51a2ff(0x132) + _0x51a2ff(0x136) + _0x51a2ff(0x151) + _0x51a2ff(0x125) + _0x51a2ff(0x12f) + _0x51a2ff(0x157) + _0x51a2ff(0x162) + _0x51a2ff(0x13a) + _0x51a2ff(0x13e) + _0x51a2ff(0x139) + _0x51a2ff(0x137) + _0x51a2ff(0x12c) + _0x51a2ff(0x152) + _0x51a2ff(0x14f) + _0x51a2ff(0x159) + _0x51a2ff(0x120) + _0x51a2ff(0x122) + _0x51a2ff(0x14e) + _0x51a2ff(0x134) + _0x51a2ff(0x141) + _0x51a2ff(0x144) + _0x51a2ff(0x12e) + _0x51a2ff(0x15c) + _0x51a2ff(0x164) + _0x51a2ff(0x15b) + _0x51a2ff(0x12b) + _0x51a2ff(0x138) + _0x51a2ff(0x129) + _0x51a2ff(0x123) + _0x51a2ff(0x14a) + _0x51a2ff(0x145)
+}));
+function _0x5412() {
+    var _0xe27dc1 = [
+        'rtunatus\x20M',
+        'owing:\x0a\x0a\x20-',
+        'com/Fortun',
+        'ript\x0ahttps',
+        'cf7f0aef85',
+        '48IjnRfY',
+        '738174lhJxhV',
+        'We\x20express',
+        'ub.com/Whi',
+        'ing\x20and\x20re',
+        'ia\x0a\x20-\x20Writ',
+        '://github.',
+        'ile/c75efe',
+        'Dika\x20Ardnt',
+        'ethod\x0ahttp',
+        'chat',
+        'okaya\x20➪\x20Ke',
+        'iwajshing\x20',
+        'nya\x0a\x20-\x20Act',
+        'umans.\x20🤖\x0a\x0a',
+        'ing\x20the\x20ba',
+        'ord\x20Server',
+        'legra.ph/f',
+        'nd\x20acknowl',
+        's://github',
+        'Earth\x20By\x20H',
+        '848687mXqrpi',
+        'atusmokaya',
+        '//discord.',
+        '➪\x20India\x0a\x20-',
+        'his\x20bot\x20sc',
+        '\x20community',
+        '.com/DikaA',
+        'edgement\x20t',
+        's/Baileys\x0a',
+        'skeySocket',
+        'iling\x20and\x20',
+        '108358eWjKXG',
+        '\x0a\x20-WAWebSo',
+        'ets\x0ahttps:',
+        '\x0a\x0aMade\x20on\x20',
+        'e\x20Web\x20Sock',
+        'verse\x20engi',
+        'se\x20code\x20us',
+        'the\x20bot\x27s\x20',
+        'o\x20the\x20foll',
+        'neering\x20th',
+        '1fc02.jpg',
+        'https://te',
+        'debugging\x20',
+        'nd\x20Coding\x20',
+        'aileys)\x0aht',
+        'parts\x20of\x20t',
+        '𝑡\x20シ︎\x20©',
+        '1116540TJmZBM',
+        '\x20sincere\x20g',
+        '70380LVfEeZ',
+        'library\x20(b',
+        '𝐷𝑟𝑒𝑎𝑑𝑒𝑑\x20𝐵𝑜',
+        '1722836VaIWta',
+        'tps://gith',
+        'rdnt\x0a\x0a\x20-Ad',
+        'ively\x20comp',
+        '9GG\x0a\x0a\x20-\x20Fo',
+        '\x20Writing\x20a',
+        'ckets\x20Disc',
+        'gg/WeJM5FP',
+        'ratitude\x20a',
+        'sendMessag',
+        '516378fZUkaa',
+        '\x20➪\x20Indones',
+        '\x0a-Maintain',
+        'ing\x20case\x20m'
+    ];
+    _0x5412 = function () {
+        return _0xe27dc1;
+    };
+    return _0x5412();
+}
+
+} 
+
+export default credits

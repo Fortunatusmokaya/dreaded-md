125f115e2ca78c8df7ef7db3225be2697b5b263d
Fortunatus Mokaya ☠️ <nyangichafortunatus@gmail.com>
2024-03-23 16:42:05 +0300

    Update join.js

--- commands/owner/join.js
+++ commands/owner/join.js
@@ -1,9 +1,9 @@
 const joinGC = async (client, m, mime, Owner, NotOwner, text, args) => {
 
-  if (!Owner) return m.reply("this command is specific to my developer and co-developer!")
+  if (!Owner) return m.reply("this command is specific to my developer...")
                  if (!text) return m.reply("provide a valid group link")
                 let result = args[0].split('https://chat.whatsapp.com/')[1] 
-                 await client.groupAcceptInvite(result).then((res) =>  m.reply(jsonformat(res))).catch((err) =>m.reply(`Link has problem.`)) 
+                 await client.groupAcceptInvite(result).then((res) =>  m.reply(jsonformat(res))).catch((err) =>m.reply(`Trying to join...`)) 
   
              } 
 

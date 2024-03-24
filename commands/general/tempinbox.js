8227ea0eae715ecbb00b6cf06a189eb1746f7037
Fortunatus Mokaya ☠️ <nyangichafortunatus@gmail.com>
2024-03-24 15:25:10 +0300

    Update general

---
+++ commands/general/tempinbox.js
@@ -0,0 +1,49 @@
+const tempI = async(client, m, text) => {
+
+
+if (!text) return m.reply("To fetch messages from your temp mail, provide the email address which was issued.")
+
+const mail = encodeURIComponent(text);
+        const checkMail = `https://tempmail.apinepdev.workers.dev/api/getmessage?email=${mail}`;
+
+try {
+            const response = await fetch(checkMail);
+
+if (!response.ok) {
+
+                return m.reply(`${response.status} error occurred while communicating with API.`);
+            }
+
+const data = await response.json();
+
+            if (!data || !data.messages) {
+
+                return m.reply('I am unable to fetch messages from your mail, your inbox might be empty or some other error occurred.');
+            }
+
+const messages = data.messages;
+
+            for (const message of messages) {
+                const sender = message.sender;
+                const subject = message.subject;
+                const date = new Date(JSON.parse(message.message).date).toLocaleString();
+                const messageBody = JSON.parse(message.message).body;
+
+                const mailMessage = `👥 Sender: ${sender}\n📝 Subject: ${subject}\n🕜 Date: ${date}\n📩 Message: ${messageBody}`;
+
+                await m.reply(mailMessage);
+            }
+        } catch (error) {
+            console.error('Error occured.');
+
+            return m.reply('something went wrong.');
+        }
+
+
+
+
+
+
+}
+
+export default tempI;

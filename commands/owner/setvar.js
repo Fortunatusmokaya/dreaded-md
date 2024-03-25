4c36d06621bcec15de4bfc61d126bb8804b26094
Fortunatus Mokaya ☠️ <nyangichafortunatus@gmail.com>
2024-03-25 10:59:10 +0300

    Update setvar.js

--- commands/owner/setvar.js
+++ commands/owner/setvar.js
@@ -4,19 +4,20 @@ const Setvar = async(client, m, text, Owner, NotOwner, herokuapi, appname, arg) 
 
 if (!Owner) return m.reply(NotOwner);
 
-if(!arg[0] || !(arg.join('').split('='))) {m.reply('Wrong format. Example\nsetvar PREFIX=!');return};
 
-const heroku = new Heroku({
-                                token: herokuapi,
-                        });
-let baseURI = "/apps/" + appname;
-        await heroku.patch(baseURI + "/config-vars", {
-          body: {
-                  [text.split('=')[0]]: text.split('=')[1],
-          },
-        });
+ if(!text.split('=')[1]) return m.reply('Incorrect Usage:\nProvide the key and value correctly\nExample: setvar ANTIBOT=TRUE')  
+ const herok = new Heroku({  
+            token: herokuapi,  
+          });  
+          let baseURI = "/apps/" + appname;  
+ await herok.patch(baseURI + "/config-vars", {  
+            body: {  
+                    [text.split('=')[0]]: text.split('=')[1],  
+            },  
+ });  
+          await m.reply(`✅ The variable ${text.split('=')[0]} = ${text.split('=')[1]} has been set Successfuly.\nBot is restarting...`);  
 
-await m.reply('Settings applied, bot is rebooting...');
+
 
 }
 

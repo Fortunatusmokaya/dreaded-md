af352523cd9e05706ed4dc4e2557411b556ac4c7
Fortunatus Mokaya ☠️ <nyangichafortunatus@gmail.com>
2024-03-25 10:34:42 +0300

    Update getvar.js

--- commands/owner/getvar.js
+++ commands/owner/getvar.js
@@ -4,9 +4,6 @@ const Getvar = async(client, m, text, Owner, NotOwner, herokuapi, appname) => {
 
 if (!Owner) return m.reply(NotOwner);
 try {
-const heroku = new Heroku({
-                                token: herokuapi,
-                        });
 
 const heroku = new Heroku({
               token: herokuapi,

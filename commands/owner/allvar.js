1f6c2fe1fe68fec785328e8aa44fcb8cbdb16579
Fortunatus Mokaya ☠️ <nyangichafortunatus@gmail.com>
2024-03-25 10:03:02 +0300

    Update owner

---
+++ commands/owner/allvar.js
@@ -0,0 +1,23 @@
+import Heroku from "heroku-client";
+
+const Allvar = async(client, m, text, Owner, NotOwner, herokuapi, appname) => {
+
+if (!Owner) return m.reply(NotOwner);
+
+const heroku = new Heroku({
+                                token: herokuapi,
+                        });
+
+let baseURI = "/apps/" + appname;
+
+let mok = await heroku.get(baseURI+'/config-vars')
+
+let mf = '*Current Heroku Variables*\n\n'
+for (dread in mok) {
+mf+= '📝 *'+dread+'* '+'= '+mok[dread]+'\n'
+}
+ m.reply(mf)
+
+}
+
+export default Allvar;

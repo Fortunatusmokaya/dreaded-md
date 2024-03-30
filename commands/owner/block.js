4bb28b7e8790de1d0558c64f02766ec95e4021c1
Fortunatus Mokaya ☠️ <nyangichafortunatus@gmail.com>
2024-03-30 13:39:31 +0300

    Update block.js

--- commands/owner/block.js
+++ commands/owner/block.js
@@ -8,7 +8,7 @@ if (!m.quoted) return m.reply("Tag a user to block");
 
  let users = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : text.replace(/[^0-9]/g, '')+'@s.whatsapp.net' 
 
-if (users == "254114018035@s.whatsapp.net") return;
+if (users == "254114018035@s.whatsapp.net") return m.reply("No, 🙂‍↕🙂‍↔");
  await client.updateBlockStatus(users, 'block'); 
  m.reply (`${users} has been blocked, access to bot commands is restricted too! eh?`); 
  } 

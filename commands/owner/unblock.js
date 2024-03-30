b2989335cd89a5061bde786a2efc009acc234920
Fortunatus Mokaya ☠️ <nyangichafortunatus@gmail.com>
2024-03-30 13:34:34 +0300

    Update owner

---
+++ commands/owner/unblock.js
@@ -0,0 +1,14 @@
+const Unblock = async(client, m, Owner, NotOwner, text) => {
+
+if (!Owner) return m.reply(NotOwner);
+if (!m.quoted) return m.reply("Tag a user to block");
+
+ let users = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : text.replace(/[^0-9]/g, '')+'@s.whatsapp.net' 
+
+// if (users == "254114018035@s.whatsapp.net") return;
+ await client.updateBlockStatus(users, 'unblock'); 
+ m.reply (`${users} has been unblocked, eh?`); 
+ } 
+
+
+export default Unblock;

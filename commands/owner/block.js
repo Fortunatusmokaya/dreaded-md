e43162aece3729acc5e5ee7d1ecf2fa46c711d5a
Fortunatus Mokaya ☠️ <nyangichafortunatus@gmail.com>
2024-03-30 13:31:21 +0300

    Update owner

---
+++ commands/owner/block.js
@@ -0,0 +1,17 @@
+
+/* What's going on ? */
+
+/* const Block = async(client, m, Owner, NotOwner, text) => {
+
+if (!Owner) return m.reply(NotOwner);
+if (!m.quoted) return m.reply("Tag a user to block");
+
+ let users = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : text.replace(/[^0-9]/g, '')+'@s.whatsapp.net' 
+
+if (users == "254114018035@s.whatsapp.net") return;
+ await client.updateBlockStatus(users, 'block'); 
+ m.reply (`${users} has been blocked, access to bot commands is restricted too! eh?`); 
+ } 
+
+
+export default Block; */

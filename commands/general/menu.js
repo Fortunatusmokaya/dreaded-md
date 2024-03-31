3935277c59e92a55e748bee3cc6f4d802dfa67cf
Fortunatus Mokaya ☠️ <nyangichafortunatus@gmail.com>
2024-03-31 14:28:04 +0300

    Update menu.js

--- commands/general/menu.js
+++ commands/general/menu.js
@@ -1,6 +1,9 @@
 import fs from "fs";
 
-const getMenu = async (client, m, menureply, botname, mode) => {
+const menu = process.env.MENUTYPE || 'LINK';
+const menuimage = process.env.MENULINK || 'https://telegra.ph/file/d6dab955fbaa42fce2280.jpg';
+
+const getMenu = async (client, m, menureply, botname, mode, video, pict) => {
   try {
     const categories = ['general', 'ai', 'coding', 'download', 'group', 'owner', 'edit'];
    
@@ -23,7 +26,39 @@ let menuText = `\nBOTNAME: ${botname}\n\nMODE: ${mode}:\n\n`;
     }
 
    // await client.sendMessage(m.chat, menuText, { quoted: m });
-menureply(menuText);
+
+
+if (menu === 'VIDEO') {
+
+                   client.sendMessage(m.chat, {
+                        video: video,
+                        caption: menuText,
+                        gifPlayback: true
+                    }, {
+                        quoted: m
+                    })
+                } else if (menu === 'IMAGE') {
+client.sendMessage(m.chat, { image: { url: menuimage }, caption: menuText, fileLength: "99999"}, { quoted: m })
+} else if (menu === 'LINK') {
+client.sendMessage(m.chat, {
+                        text: menuText,
+                        contextInfo: {
+                            externalAdReply: {
+                                showAdAttribution: true,
+                                title: `DREADED-MD`,
+                                body: `fff`,
+                                thumbnail: pict,
+                                sourceUrl: ``,
+                                mediaType: 1,
+                                renderLargerThumbnail: true
+                            }
+                        }
+                    }, {
+                        quoted: m
+                    })
+
+}
+else { menureply(menuText); }
   } catch (error) {
     console.error(error);
     m.reply('An error occurred while fetching the menu.');

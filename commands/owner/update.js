5f473087fa7dd9b7e7110ee130b7697c8819339e
Fortunatus Mokaya ☠️ <nyangichafortunatus@gmail.com>
2024-03-31 14:50:27 +0300

    Update update.js

--- commands/owner/update.js
+++ commands/owner/update.js
@@ -13,26 +13,10 @@ await git.fetch();
 if (commits.total === 0) {        
                 return m.reply('✅ Your bot is up to date with main branch!') }
 
-else {        
-                    var app = await heroku.get('/apps/' + appname ) 
+else { m.reply("❌ Your bot is not up to date, update it by redeploying or syncing your fork")
 
-git.fetch('upstream', 'main');        
-                  git.reset('hard', ['FETCH_HEAD']);
-
-var git_url = app.git_url.replace(        
-                    "https://", "https://api:" + herokuapi + "@"        
-                  ) 
-
-try {        
-                    await git.addRemote('heroku', git_url);        
-                  } catch (error) { m.reply('An error occurred while pushing latest commits to your bot. Redeploy the bot manually.'); }  
-
-
-await git.push('heroku', 'main');   
-
-await m.reply('✅ Your bot has been updated.\n\nRestarting bot...')       
-                        
-}
+}   
+                    
 };
 
 export default Update;

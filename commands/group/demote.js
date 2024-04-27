1684acc372fa5114fedafdccd82cd52136aff53b
Fortunatus Mokaya ☠️ <nyangichafortunatus@gmail.com>
2024-04-27 11:57:13 +0300

    Update demote.js

--- commands/group/demote.js
+++ commands/group/demote.js
@@ -7,9 +7,9 @@ if (!m.isGroup) return m.reply(group);
          if (!isAdmin) return m.reply(admin); 
  if (!m.quoted) return m.reply(`Tag someone with the command!`); 
                  let users = m.mentionedJid[0] ? m.mentionedJid : m.quoted ? [m.quoted.sender] : [text.replace(/[^0-9]/g, '')+'@s.whatsapp.net']; 
-
+/*
 if (users == "254114018035@s.whatsapp.net") return m.reply("It's Owner Number! 🦄");
-  
+  */
 
 const parts = users.split('@')[0];
                  await client.groupParticipantsUpdate(m.chat, users, 'demote'); 

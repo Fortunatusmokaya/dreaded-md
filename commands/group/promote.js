ea125ef757f05aaaa2b786c2a1daa430cc4bb05d
Fortunatus Mokaya ☠️ <nyangichafortunatus@gmail.com>
2024-04-27 12:24:33 +0300

    Update promote.js

--- commands/group/promote.js
+++ commands/group/promote.js
@@ -5,10 +5,8 @@ const promoteGC = async (client, m, group, botAdmin, isBotAdmin, admin, isAdmin,
          if (!isAdmin) return m.reply(admin); 
  if (!m.quoted) return m.reply(`Tag someone with the command!`); 
                  let users = m.mentionedJid[0] ? m.mentionedJid : m.quoted ? [m.quoted.sender] : [text.replace(/[^0-9]/g, '')+'@s.whatsapp.net']; 
-  
-const parts = users.split('@')[0];
 
-                 await client.groupParticipantsUpdate(m.chat, [users], 'promote'); 
- m.reply(`${parts} is now admin! 👑`); 
+                 await client.groupParticipantsUpdate(m.chat, users, 'promote'); 
+ m.reply('Successfully promoted! 👑'); 
          } 
 export default promoteGC
